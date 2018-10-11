46f75d097b9a09404f959e04030648227d7f78c3
khapeterk <khapeterk@gmail.com>
2018-10-10 20:51:49 -0400

    chapter 3 page 84 rock.py

--- Chapter3/rock.py
+++ Chapter3/rock.py
@@ -1,4 +1,11 @@
 import random
 
 random_choice = random.randint(0,2)
-print('The computer chooses', random_choice)
+if random_choice == 0:
+    computer_choice = 'rock'
+elif random_choice == 1:
+    computer_choice = 'paper'
+elif random_choice == 2:
+    computer_choice = 'scissors'
+
+print ('The computer chooses', computer_choice)
