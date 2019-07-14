99f8d1d2c04522041920b19ba23df3084f0cfd2d
khapeterk <khapeterk@gmail.com>
2019-07-14 11:59:52 -0400

    chapter 4 page 161 bubbles.py

--- Chapter4/bubbles.py
+++ Chapter4/bubbles.py
@@ -4,7 +4,20 @@ scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
 
 i = 0
 length = len(scores)
+high_score = 0
 
 for i in range(length):
     print('Bubble solution #' + str(i), 'score:', scores[i])
-    i = i + 1
+    if scores[i] > high_score:
+        high_score = scores[i]
+
+print('Bubbles tests:', length)
+print('Highest bubble score:', high_score)
+
+best_solutions = []
+for i in range(length):
+    if scores[i] == high_score:
+        best_solutions.append(i)
+
+print('Solutions with the highest score:', best_solutions)
+                              
